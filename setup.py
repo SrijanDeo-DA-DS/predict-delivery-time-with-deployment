6c835cf3e4f7f8a07ffd50fd06a9a1e3797e6bae
SrijanDeo-DA-DS <sd759@njit.edu>
2024-10-11 22:28:46 -0500

    add project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to predict the delivery time of a food item using Regression ML algorithms. It will use all Mlops principles to deploy the model into production',
+    author='srijandeo',
+    license='',
+)
